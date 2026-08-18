de273a23cef9f9eaa02efb0e27309c77c6993522
Steven0411 <112782936+Steven0411@users.noreply.github.com>
2026-08-18 09:45:45 +0100

    fixed formatting of backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "tech603-steven-project-2"
     key = "production.tfstate"
-    region = var.region
+    region = "eu-west-1"
     use_lockfile = true
   }
 }
